590f8c8b0891db1dc52f99a5e7f6ce642ab523b3
Colin Reinhart <colinbreinhartgmail.com>
2022-06-14 11:54:47 -0600

    Add tests for full model coverage

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -284,6 +284,32 @@ RSpec.describe Merchant do
       it 'returns the top 5 merchants by revenue' do
         expect(Merchant.top_5_merchants_by_revenue.to_a).to eq([@merch_5, @merch_6, @merch_4, @merch_3, @merch_1])
       end
+      describe "#top_5_items" do
+        before :each do
+          @merch_1 = Merchant.create!(name: "Two-Legs Fashion")
+          @item_1 = @merch_1.items.create!(name: "Two-Leg Pantaloons", description: "pants built for people with two legs", unit_price: 5000)
+          @item_2 = @merch_1.items.create!(name: "Two-Leg Shorts", description: "shorts built for people with two legs", unit_price: 3000)
+          @item_3 = @merch_1.items.create!(name: "Hat", description: "hat built for people with two legs and one head", unit_price: 6000)
+          @item_4 = @merch_1.items.create!(name: "Double Legged Pant", description: "pants built for people with two legs", unit_price: 50000)
+          @item_5 = @merch_1.items.create!(name: "Stainless Steel, 5-Pocket Jean", description: "Shorts of Steel", unit_price: 3000000)
+          @item_6 = @merch_1.items.create!(name: "String of Numbers", description: "54921752964273", unit_price: 100)
+          @invoice_1 = @cust_1.invoices.create!(status: 2)
+          @invoice_2 = @cust_1.invoices.create!(status: 2)
+          @invoice_3 = @cust_1.invoices.create!(status: 2)
+          @invoice_4 = @cust_1.invoices.create!(status: 2)
+          @invoice_5 = @cust_1.invoices.create!(status: 2)
+          @invoice_6 = @cust_1.invoices.create!(status: 2)
+          @ii_1 = InvoiceItem.create!(item_id: @item_1.id, invoice_id: @invoice_1.id, quantity: 6, unit_price: @item_1.unit_price, status: 2)
+          @ii_2 = InvoiceItem.create!(item_id: @item_2.id, invoice_id: @invoice_2.id, quantity: 5, unit_price: @item_1.unit_price, status: 2)
+          @ii_3 = InvoiceItem.create!(item_id: @item_3.id, invoice_id: @invoice_3.id, quantity: 4, unit_price: @item_1.unit_price, status: 2)
+          @ii_4 = InvoiceItem.create!(item_id: @item_4.id, invoice_id: @invoice_4.id, quantity: 3, unit_price: @item_1.unit_price, status: 2)
+          @ii_5 = InvoiceItem.create!(item_id: @item_5.id, invoice_id: @invoice_5.id, quantity: 2, unit_price: @item_1.unit_price, status: 2)
+          @ii_6 = InvoiceItem.create!(item_id: @item_6.id, invoice_id: @invoice_6.id, quantity: 1, unit_price: @item_1.unit_price, status: 2)
+        end
+        it "returns the top 5 items" do
+          expect(@merch_1.top_5_items).to eq([@item_1, @item_2, @item_3, @item_4, @item_5])
+        end
+      end
     end
   end
 
